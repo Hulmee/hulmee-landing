315ac6c72f672d0be76a52fbabd83e09307cf72c
Ellery Hulme <ellery.hulme@outlook.com>
2024-07-07 21:53:51 +1000

    updated alt text

--- components/Profile.vue
+++ components/Profile.vue
@@ -4,7 +4,8 @@
       <div class="mask mask-squircle w-28 glass p-2 ">
         <!-- <div class="ring-accent  ring-offset-base-100 w-24 rounded-full ring ring-offset-2"> -->
 
-        <img src="../public/avatar.png" alt="Avatar" />
+        <img src="../public/avatar.png"
+          alt="Ellery Hulme Bitmoji Avatar with dark sunglasses, brown hair, and a beard wearing a black jacket with a fur collar." />
       </div>
     </div>
 
